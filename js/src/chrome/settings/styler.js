a11edc21cddd1f0b98033af5404ac58ed77ec88f
knutole <knutolesjoli@gmail.com>
2016-02-12 13:22:01 +0100

    updated route createTileLayer, fixes #59

--- js/src/chrome/settings/styler.js
+++ js/src/chrome/settings/styler.js
@@ -1150,7 +1150,7 @@ Wu.Styler = Wu.Class.extend({
 		};
 
 		// create layer on server
-		app.api.createLayer(layerJSON, function (err, newLayerJSON) {
+		app.api.createTileLayer(layerJSON, function (err, newLayerJSON) {
 			if (err) {
 				return app.feedback.setError({
 					title : 'Something went wrong',
