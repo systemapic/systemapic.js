6d7f54837403fa56dd0d5fe4c0c4d3aab386fb94
knutole <knutolesjoli@gmail.com>
2016-03-10 17:37:06 +0100

    Generated docs

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -5,13 +5,13 @@ define({
   "header": {
     "title": "Systemapic API"
   },
-  "sampleUrl": "https://dev.systemapic.com",
   "version": "1.3.5",
   "description": "bringing geographical data to life",
+  "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-02-21T19:22:03.784Z",
+    "time": "2016-03-10T13:29:25.938Z",
     "url": "http://apidocjs.com",
     "version": "0.14.0"
   }
